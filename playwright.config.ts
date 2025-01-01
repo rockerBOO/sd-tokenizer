3ec745fa5ced739f9c7644919c7bd28eb6011c3c
rockerBOO <rockerboo@gmail.com>
2025-01-01 17:31:11 -0500

    Try some more playwright things

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,48 +1,49 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
- projects: [
+  projects: [
     /* Test against desktop browsers */
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
     /* Test against mobile viewports. */
     {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
     },
     {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
     },
     /* Test against branded browsers. */
     {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' }, // or 'chrome-beta'
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" }, // or 'chrome-beta'
     },
     {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' }, // or 'msedge-dev'
+      name: "Microsoft Edge",
+      use: { ...devices["Desktop Edge"], channel: "msedge" }, // or 'msedge-dev'
     },
   ],
   // Run your local dev server before starting the tests
   webServer: {
-    command: 'yarn run dev',
-    url: 'http://127.0.0.1:5173',
+    command: "yarn run preview",
+    url: "http://127.0.0.1:5173",
     reuseExistingServer: !process.env.CI,
-    stdout: 'ignore',
-    stderr: 'pipe',
+    stdout: "ignore",
+    stderr: "pipe",
   },
+  workers: process.env.CI ? 1 : undefined,
   use: {
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:5173",
   },
 });
