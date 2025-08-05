ac361c8731d19694866d00517fd5b04badd1611c
Dave Lage <rockerboo@gmail.com>
2025-08-05 16:44:03 -0400

    Add tests, refactor for multiple tokenizers (#2)
    * Add tests, refactor for multiple tokenizers
    
    * Update playwright install
    
    * Bump lock, update yarn
    
    * Update workflow
    
    * Try some more playwright things
    
    * Remove container
    
    * dev
    
    * :)
    
    * Set version
    
    * Update playwright
    
    * Set tests directory
    
    * Lets see

--- README.md
+++ README.md
@@ -12,6 +12,43 @@ https://sd-tokenizer.rocker.boo/
 wasm-pack build --target no-modules
 ```
 
+## Usage
+
+After building the WASM we can load everything from an http-server. 
+
+Example server:
+
+```
+npx http-server
+```
+```
+npx http-server
+Starting up http-server, serving ./
+
+http-server version: 14.1.1
+
+http-server settings:
+CORS: disabled
+Cache: 3600 seconds
+Connection Timeout: 120 seconds
+Directory Listings: visible
+AutoIndex: visible
+Serve GZIP Files: false
+Serve Brotli Files: false
+Default File Extension: none
+
+Available on:
+  http://127.0.0.1:8080
+  http://192.168.1.7:8080
+  http://172.21.0.1:8080
+  http://172.19.0.1:8080
+  http://172.18.0.1:8080
+Hit CTRL-C to stop the server
+```
+
+And we can go to `http://127.0.0.1:8080`.
+ 
+
 ## Deploy
 
 Using fly to host the service
---
+++ playwright.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "e2e-tests",
+  projects: [
+    /* Test against desktop browsers */
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    /* Test against mobile viewports. */
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
+    /* Test against branded browsers. */
+    {
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" }, // or 'chrome-beta'
+    },
+    {
+      name: "Microsoft Edge",
+      use: { ...devices["Desktop Edge"], channel: "msedge" }, // or 'msedge-dev'
+    },
+  ],
+  // Run your local dev server before starting the tests
+  webServer: {
+    command: "yarn run dev",
+    url: "http://localhost:5173",
+    reuseExistingServer: !process.env.CI,
+    stdout: "ignore",
+    stderr: "pipe",
+  },
+  workers: process.env.CI ? 1 : undefined,
+  use: {
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:5173",
+  },
+});
